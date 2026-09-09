25e1cbf55c3dc39266853ab7fcbe50446359d8b1
Giacomo Queirolo (cosma) <dc-quei1@login8b.pri.cosma.local>
2026-09-09 17:51:07 +0100

    test_vdisp : correction of v_disp in colibre, correction of kappa_max, and further study of theta_E/theta_E_vdisp in colibre galaxies, and their relation w the core

--- src/nazgul/test_vdisp.py
+++ src/nazgul/test_vdisp.py
@@ -27,7 +27,7 @@ from nazgul.Translator import std_sim,std_simsuite,std_subsim
 from nazgul.Translator.EAGLE.particle_galaxy import compute_principal_axes
 from nazgul.project_gal import project_Gal
 
-def get_tEsis(lensgal,**kwargs_query):
+def get_tE_SIS(lensgal,**kwargs_query):
     lensgal.unpack()
     cosmo = lensgal.cosmo
     Ds    = cosmo.angular_diameter_distance(lensgal.z_source)
@@ -40,21 +40,20 @@ def get_tEsis(lensgal,**kwargs_query):
                             simpartgal=lensgal.Gal,
                             **kwargs_query)
     
-    theta_E_sis = get_tE_sis(vdisp_stars,Dls=Dls,Ds=Ds)
+    theta_E_sis = _get_tE_SIS(vdisp_stars,Dls=Dls,Ds=Ds)
     return theta_E_sis
 
-def get_tE_sis(v_disp,Dls,Ds):
+def _get_tE_SIS(v_disp,Dls,Ds):
     vdis_c_ratio = v_disp/const.c
     theta_E_sis_radns = 4*np.pi*(vdis_c_ratio.value)*Dls/Ds
     theta_E_sis = theta_E_sis_radns.value*u.radian.to("arcsec")*u.arcsec
     return theta_E_sis    
 
-def get_kw_tE(out_dll="tmp/del_theta_sis.dll",
-              kw_get_gallens={},
+def get_kw_tE(lenses,
+              out_dll="tmp/del_theta_sis.dll",
               kwargs_query={},
              correct_core=True,
              compute_q=True):
-    lenses  = get_all_gallens(**kw_get_gallens)
     theta_E_list = []
     theta_sis_list = []
     zs = []
@@ -65,7 +64,7 @@ def get_kw_tE(out_dll="tmp/del_theta_sis.dll",
         q_list = []
     for lensgal in lenses:
         lensgal.unpack()
-        theta_E_sis = get_tEsis(lensgal,**kwargs_query)
+        theta_E_sis = get_tE_SIS(lensgal,**kwargs_query)
         theta_sis_list.append(theta_E_sis.value)
         theta_E_list.append(lensgal.thetaE.value)
         zs.append(lensgal.z_lens)
@@ -73,13 +72,20 @@ def get_kw_tE(out_dll="tmp/del_theta_sis.dll",
         print("tE_sis=",np.round(theta_E_sis,2))
         print("Ratio of theta_E computed vs theta_E_sis from vel. disp:",np.round(lensgal.thetaE/theta_E_sis,2))
         if correct_core:
+            # MD is actually obtained by averaging 5% of the most massive cells - its center is very stable and fairly accurate, but the MD value ends up being taken as weighted average over > theta_E^2 area -> not an accurate rep. of the maximum kappa
+            # take instead the max kappa
+            """
             try:
                 kw_prj = load_whatever(lensgal.Gal.projection_path)
             except FileNotFoundError:
                 # recomputing 
                 kw_prj  = project_Gal(lensgal.Gal,z_source_max=lensgal.z_source_max,sample_z_source =lensgal.sample_z_source,min_thetaE=lens.min_thetaE)
             kmax = kw_prj["MD_value"]/lensgal.SigCrit
-            rcc  = np.sqrt(1-(1/kmax.value))
+            """
+            # this might still be wrong by being shot-noise dominated
+            kmax = np.max(lensgal.kappa_map)
+            
+            rcc  = np.sqrt(1-(1/kmax))
             print("Ratio of theta_E computed vs theta_E_sis from vel. disp  (core corr.):",np.round(lensgal.thetaE/(theta_E_sis*rcc),2))
             rcc_list.append(rcc)
         if compute_q:
@@ -122,6 +128,7 @@ def get_kw_tE(out_dll="tmp/del_theta_sis.dll",
         kw_tE["q"] = np.array(q_list)
     with open(out_dll,"wb") as f:
         dill.dump(kw_tE,f)
+    print(f"Saving {out_dll}")
     return kw_tE
     
 def comp_tE_vs_tEsis(reload=True,
@@ -141,10 +148,12 @@ def comp_tE_vs_tEsis(reload=True,
             print(f"Failed to load {out_dll}")
             reload = False
     if reload == False:
-        kw_tE = get_kw_tE(kw_get_gallens=kw_get_gallens,
+        lenses  = get_all_gallens(verbose=False,**kw_get_gallens)
+
+        kw_tE = get_kw_tE(lenses=lenses,
+                          out_dll=out_dll,
                           kwargs_query=kwargs_query,
-                         out_dll=out_dll,
-                         correct_core=correct_core,
+                          correct_core=correct_core,
                           compute_q=compute_q)
     theta_sis_list = kw_tE["theta_E_sis"]
     theta_E_list   = kw_tE["theta_E"]
@@ -157,7 +166,6 @@ def comp_tE_vs_tEsis(reload=True,
     plt.suptitle(title)
     ax = axis[0]
     sis_range = theta_sis_list.min(),theta_sis_list.max()
-    tE_range  = theta_E_list.min(),theta_E_list.max()
     sis_11 = np.linspace(*sis_range,10)
     tE_11 = np.linspace(*sis_range,10)
     ax.plot(sis_11,tE_11,ls="--",c="k",label="1:1")
@@ -170,7 +178,7 @@ def comp_tE_vs_tEsis(reload=True,
     fig.colorbar(im0, cax=cax, orientation='vertical',label=r"z$_{lens}$")
     ax.axis('equal')
     ax.set_ylabel(r"$\theta_E$")
-    ax.set_xlabel(r"$\theta_E(v_{disp})$")
+    ax.set_xlabel(r"$\theta_E(\sigma_{\rm{v}})$")
     ax.legend()
     
     ax = axis[1]
@@ -186,7 +194,7 @@ def comp_tE_vs_tEsis(reload=True,
     ax.plot(x,np.ones_like(x),ls="--",c="k",label="1")
     
     ax.set_xlabel(r"Gal")
-    ax.set_ylabel(r"$\theta_E$/$\theta_E(v_{disp})$")
+    ax.set_ylabel(r"$\theta_E$/$\theta_E(\sigma_{\rm{v}})$")
     ax.legend()
     plt.tight_layout()
     plt.savefig(name_plot)
@@ -198,10 +206,11 @@ def comp_tE_vs_tEsis(reload=True,
         nbins =10
         print("N=",len(tE_ratio))
         plt.hist(tE_ratio,bins=nbins,alpha=.5,color="b",label=r"Original N="+str(len(tE_ratio)))
-        plt.axvline(np.nanmedian(tE_ratio),c="b",ls="--",label=r"$\theta_E/\theta_{E,SIS}$="+str(np.round(np.nanmedian(tE_ratio),1)))
+        plt.axvline(np.nanmedian(tE_ratio),c="b",ls="--",label=r"$\theta_E/\theta_{\rm{E, SIS}}(\sigma_{\rm{v}})$="+str(np.round(np.nanmedian(tE_ratio),1)))
         plt.hist(tE_ratio_corr,bins=nbins,color="r",alpha=.5,label=r"$\theta_c$ correction")
-        plt.axvline(np.nanmedian(tE_ratio_corr),c="r",ls="--",label=r"$\theta_E/(\theta_{E,SIS}*Rcc)$="+str(np.round(np.nanmedian(tE_ratio_corr),1)))
+        plt.axvline(np.nanmedian(tE_ratio_corr),c="r",ls="--",label=r"$\theta_E/(\theta_{\rm{E, SIS}}(\sigma_{\rm{v}}*\rm{R}_{\rm{cc}})$="+str(np.round(np.nanmedian(tE_ratio_corr),1)))
         plt.title(r"$\theta_E$ ratio from mass map and from $\sigma_v$ with/wo cored profile correction")
+        plt.yticks([])
         plt.legend()
         plt.xlabel(r"$\theta_E/\theta_{E,SIS}$")
         plt.tight_layout()
@@ -217,8 +226,8 @@ def comp_tE_vs_tEsis(reload=True,
         y_kc = np.sqrt(1-1/x_kc)
         plt.plot(x_kc,y_kc,ls="--",c="k",label=r"f($\kappa_{\rm{max}}$) = $\sqrt{1-\frac{1}{\kappa_{\rm{max}}}}$")
         plt.xlabel(r"$\kappa_{\rm{max}}$")
-        plt.ylabel(r"$\theta_E$/$\theta_E(v_{disp})$")
-        plt.title(r"$\theta_E$/$\theta_E(v_{disp})$ wrt $\kappa_{\rm{max}}$ and fit for $\kappa_{\rm{c}}=\kappa_{\rm{max}}$")
+        plt.ylabel(r"$\theta_E$/$\theta_E(\sigma_{\rm{v}})$")
+        plt.title(r"$\theta_E$/$\theta_E(\sigma_{\rm{v}})$ wrt $\kappa_{\rm{max}}$ and fit for $\kappa_{\rm{c}}=\kappa_{\rm{max}}$")
         plt.legend()
         name_plot3 = name_plot2.replace("_2.","_kc_vs_tEratio.")
         plt.savefig(name_plot3)
@@ -226,7 +235,8 @@ def comp_tE_vs_tEsis(reload=True,
         plt.close()
         
         print("Ugly but fast: output lenses for which the correction is not sufficient and plot their kappa map")
-        lenses  = get_all_gallens(**kw_get_gallens)
+        if not reload:
+            lenses  = get_all_gallens(verbose=False,**kw_get_gallens)
         #np.where(tE_ratio_corr<thresh_tE_ratio_corr,
         if compute_q:
             fig,ax = plt.subplots(1) 
@@ -242,7 +252,7 @@ def comp_tE_vs_tEsis(reload=True,
             ax.plot(x,np.ones_like(x),ls="--",c="k",label="1")
             
             ax.set_xlabel(r"Gal")
-            ax.set_ylabel(r"$\theta_E$/$\theta_E(v_{disp})$")
+            ax.set_ylabel(r"$\theta_E$/$\theta_E(\sigma_{\rm{v}})$")
             ax.legend()
             plt.tight_layout()
             name_plot4 = name_plot2.replace("_2.","_q.")
@@ -315,6 +325,6 @@ if __name__ =="__main__":
                     kwargs_query=kwargs_query,
                     out_dll = out_dll,
                     name_plot=name_plot,
-                     correct_core=correct_core,
-                     compute_q=compute_q
+                    correct_core=correct_core,
+                    compute_q=compute_q
                     )
