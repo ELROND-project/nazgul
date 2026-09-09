23cf981239e21371fde361668498e56bbc06bdf9
Giacomo Queirolo (cosma) <dc-quei1@login8b.pri.cosma.local>
2026-09-09 16:50:08 +0100

    mount_doom/generate_gal_lens.py: minor changes (reload will not recompute the plot_AMR_density)

--- src/nazgul/mount_doom/generate_gal_lens.py
+++ src/nazgul/mount_doom/generate_gal_lens.py
@@ -88,7 +88,7 @@ class GalLens(BasicLensPart):
             self.scale_tE = (self.radius/self.thetaE).value
         if not hasattr(self,"Gal"):
             print("MONKEY_PATCH: unpacking to get Gal id") 
-            self.unpack()
+            self.unpack(verbose=False)
         _id = (
             self.Gal._identity(),
             self.PartLens._identity(),
@@ -382,17 +382,19 @@ def wrapper_forge_all_lenses(reload=True,
                     log_memory(f"after GalLens run {Gal_name}")
                     supercrit = True
                     pji = kw_lenspart["projection_index"]
-                    print(f"Projection {pji} of {Gal_name} is supercritical!\n")
-                    # Plotting density for each individual particle
-                    plot_AMR_densityXpart(Gal=mod_LP.Gal,
-                                          proj_index=pji,
-                                          savedir=mod_LP.savedir,
-                                          rerun=not reload)
+                    if not reload:
+                        print(f"Projection {pji} of {Gal_name} is supercritical!\n")
+                        # Plotting density for each individual particle
+                        plot_AMR_densityXpart(Gal=mod_LP.Gal,
+                                              proj_index=pji,
+                                              savedir=mod_LP.savedir,
+                                              rerun=not reload)
                     plt.close("all")
                     N_lenses+=1
                     del mod_LP
                     mod_LP = None
-                    log_memory(f"after plot_AMR_densityXpart run {Gal_name}")
+                    if not reload:
+                        log_memory(f"after plot_AMR_densityXpart run {Gal_name}")
 
                     if not consider_all_proj:
                         print(f"Considering only the first supercritical solution.\n")
